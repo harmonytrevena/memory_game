3e56b7603813d425220d93d0c49317e149cd1245
Harmony Trevena <harmonytrevena@gmail.com>
2020-10-07 14:29:37 -0400

    Completed Part 2: Setting up click events

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,12 +3,31 @@ import './MemoryCard.css';
 import Card from './images/Card.png';
 
 class MemoryCard extends Component {
+    constructor() {
+        super();
+        this.state = { isFlipped: false };
+    }
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        });
+    }
     render() {
-      return (
-        <div className="MemoryCard">
-            <img src={Card} className="Card-Logo" alt="Question mark" />
-        </div>
-      );
+        let memoryCardInnerClass = "MemoryCardInner";
+            if (this.state.isFlipped)
+            memoryCardInnerClass = this.state.isFlipped ? "MemoryCardInner flipped" : "MemoryCardInner";
+        return (
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img src={Card} className="Card-Logo" alt="Question mark" />
+                    </div>
+                    <div className="MemoryCardFront">
+                        <p>∆</p>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
